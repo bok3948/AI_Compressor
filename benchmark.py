619b48e07be46a1020748079531f290dfa8ea60c
curry_bale <93563329+bok3948@users.noreply.github.com>
2024-10-22 13:29:11 +0900

    Add files via upload

---
+++ benchmark.py
@@ -0,0 +1,30 @@
+from compress.engine import evaluate 
+from util.profiler import profiler
+from onnx_inference import ONNX_inference
+from prettytable import PrettyTable
+
+def Benchmark(model_type, model, model_path, data_loader_val, dummy_size, device, args):
+    #modell type: torch or onnx
+    pf = profiler(dummy_size=dummy_size, model_type=model_type)
+    if model_type == "torch":
+        torch_stats = evaluate(data_loader_val, model, device, args)
+        summary = pf.summary(model, model_path, device, args.model)
+        summary.update(torch_stats)
+    
+    elif model_type == "onnx":
+        onnx_stats = ONNX_inference(data_loader_val, model_path, args)
+        summary = pf.summary(None, model_path, device, args.model)
+        summary.update(onnx_stats)
+    else:
+        raise ValueError("Model type not supported")
+    
+    table = PrettyTable()
+
+    table.field_names = list(summary.keys())
+    table.add_row(list(summary.values()))
+    print(table)
+    
+    return summary
+
+    
+    
